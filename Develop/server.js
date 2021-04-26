1862c8018e66f139d62e32b99d7c9c5e2780085e
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-04-26 09:16:17 -0600

    setup

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routes will go here
+
+// require("./routes/apiroutes")(app);
+// require("./routes/htmlroutes")(app);
+
+// start the server
+app.listen(PORT, () => {
+    console.log('Server started! At http://localhost:' + PORT);
+});
