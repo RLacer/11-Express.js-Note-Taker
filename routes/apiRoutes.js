a14e86c28efc9f43cca0b9e3e6ef97c101803416
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-04-27 15:55:46 -0600

    new file  structure added routes and js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,1 @@
+// const tableData = require('../data/tableData');
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // routes will go here
 
 // require("./routes/apiroutes")(app);
-// require("./routes/htmlroutes")(app);
+require("./routes/htmlroutes")(app);
 
 // start the server
 app.listen(PORT, () => {
